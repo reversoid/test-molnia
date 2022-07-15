3cb0f06d37ab7455bb8971af31319a9f56aafb74
George <george.kobunov@ddgcorp.ru>
2022-07-15 22:30:44 +0300

    set up a basis of project, created two pages(add user, users) and one component for user form which we can reuse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AddUserPageComponent } from './pages/add-user-page/add-user-page.component';
+import { UsersPageComponent } from './pages/users-page/users-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add', component: AddUserPageComponent},
+  {path: 'users', component: UsersPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
